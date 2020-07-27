61329574f6d05a82c39f757328de91c1e5e679c8
Ultamatum <alexanderg0502@gmail.com>
2020-07-27 22:19:18 +0100

    Made First Version Of Extension
    Just a small thing so not a bunch of commits.
    Essentially, a command is created in VS Code called "Create P5 Project" this will create everything required for a P5 project to be built in 2 files (index.html & sketch.js).

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('p5-project-creator.createP5Project', async () => {
+		let data = await vscode.window.showInputBox({
+			placeHolder: "Project Name"
+		});
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "p5-project-creator" is now active!');
+		if (vscode.workspace.workspaceFolders && data) {
+			const jsfile = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, "/sketch.js");
+			const htmlfile = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, "/index.html");
+			const edit = new vscode.WorkspaceEdit();
+			let canGo: boolean = true;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('p5-project-creator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+			if (fs.existsSync(jsfile.fsPath)) {
+				vscode.window.showErrorMessage("sketch.js already exists in current workspace!");
+				canGo = false;
+			}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from P5 Project Creator!');
+			if (fs.existsSync(htmlfile.fsPath)) {
+				vscode.window.showErrorMessage("index.html already exists in current workspace!");
+				canGo = false;
+			}
+
+			if (canGo) {
+				edit.createFile(jsfile, { ignoreIfExists: true, overwrite: false });
+				edit.insert(jsfile, new vscode.Position(0, 0), "function setup() \n{\n\tcreateCanvas(400, 400);\n}\n\nfunction draw()\n{\n\n}\n");
+
+				edit.createFile(htmlfile, { ignoreIfExists: true, overwrite: false });
+				edit.insert(htmlfile, new vscode.Position(0, 0), "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n\t<meta charset=\"utf-8\">\n\t<title>" + `${data}` + "</title>\n\n\t<script src=\"https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/p5.js\"></script>\n\n</head>\n\n<body>\n\t<script src=\"sketch.js\"></script>\n</body>\n\n</html>\n");
+
+				vscode.workspace.applyEdit(edit);
+
+				await new Promise(resolve => setTimeout(resolve, 500));
+
+				vscode.workspace.saveAll();
+
+				vscode.window.showInformationMessage("Project Creation Complete");
+			}
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
